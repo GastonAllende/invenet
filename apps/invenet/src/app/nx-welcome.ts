39ec2987883705bdb795f15996892b2c632d7aff
Gaston Saavedra <gaston.saavedra85@gmail.com>
2026-02-07 22:00:19 +0100

    move api folder

--- apps/invenet/src/app/nx-welcome.ts
+++ apps/invenet/src/app/nx-welcome.ts
@@ -818,14 +818,13 @@ import { CommonModule } from '@angular/common';
                   <span> Enable faster CI & better DX </span>
                 </h2>
               </div>
-              <p>Your Nx Cloud remote cache setup is almost complete.</p>
-
+              <p>Your workspace is connected to Nx Cloud.</p>
               <a
-                href="https://cloud.nx.app/connect/dV7rToQYpi"
+                href="https://nx.dev/ci/intro/ci-with-nx#learn-about-nx-on-ci"
                 target="_blank"
                 rel="noreferrer"
               >
-                Click here to finish
+                Learn about Nx on CI
               </a>
             </div>
           </div>
